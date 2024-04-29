e5a03b43323ad7179d74e4a637cb693af44cb2e6
andika <zoneandika@gmail.com>
2024-04-29 21:12:20 +0700

    fix navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 export default function Navbar() {
   const navbarRef = useRef<HTMLDivElement>(null);
   const [ontop, setOntop] = useState(true);
+  const [navbarShowed, setNavbarShowed] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -59,19 +60,48 @@ export default function Navbar() {
         <img
           src={`hamburger-${ontop ? "white" : "black"}.svg`}
           className="w-[20px]"
-          alt=""
+          onClick={() => {
+            setNavbarShowed(true);
+          }}
         />
       </div>
 
-      <div className="h-screen text-black bg-white shadow-lg fixed right-0 bottom-0 py-[150px] px-[50px] w-[80%] text-center">
+      <div
+        className={`h-screen fixed bottom-0 text-black bg-white shadow-lg py-[150px] px-[50px] w-[80%] text-center duration-300 transition right-0 ${
+          navbarShowed ? "translate-x-0" : "translate-x-[150%]"
+        }`}
+      >
+        <button>
+          <img
+            src="cross.svg"
+            className={`fixed top-[50px] w-[20px] duration-300 transition ${
+              navbarShowed ? "right-[30px]" : "-right-[100px]"
+            }`}
+            onClick={() => {
+              setNavbarShowed(false);
+            }}
+          />
+        </button>
         <div className="flex flex-col gap-[30px]">
-          <Link href={"/"} className=" underline font-figtree font-bold">
+          <Link
+            href={"/"}
+            onClick={() => setNavbarShowed(false)}
+            className=" underline font-figtree font-bold"
+          >
             Home
           </Link>
-          <Link href={"/paket"} className=" underline font-figtree font-bold">
+          <Link
+            href={"/paket"}
+            onClick={() => setNavbarShowed(false)}
+            className=" underline font-figtree font-bold"
+          >
             product
           </Link>
-          <Link href={"#"} className=" underline font-figtree font-bold">
+          <Link
+            href={"#"}
+            onClick={() => setNavbarShowed(false)}
+            className=" underline font-figtree font-bold"
+          >
             about
           </Link>
         </div>
